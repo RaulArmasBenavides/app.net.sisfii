b202352026f3f17e1c7a8ef61e5b0db53844f1fd
Raúl Armas <raularmasbx@gmail.com>
2022-04-19 23:20:30 -0500

    cambions

--- app.net.sisfii/CapaPresentacion/Util/Helper.cs
+++ app.net.sisfii/CapaPresentacion/Util/Helper.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -30,6 +32,142 @@ namespace CapaPresentacion.Util
             Bitmap bm = new Bitmap(mStream, false);
             mStream.Dispose();
             return bm;
+        }
+
+        public static string ObtenerRutaTemporalUsuario()
+        {
+            return Environment.ExpandEnvironmentVariables("%temp%\\");
+        }
+
+        public static string ObtenerRutaTemporalWindows()
+        {
+            return Environment.ExpandEnvironmentVariables("%systemroot%\\TEMP\\");
+        }
+
+
+        public static string FormatoFecha(string psFecha_mdy)
+        {   // OBJETIVO : Modificar el formato de una fecha de MM/DD/YYYY a DD/MM/YYYY o viceversa
+            //SOLICITA  : psFecha_mdy = Fecha a formatear
+            //RETORNA   : Fecha formateada
+
+            if (psFecha_mdy.Equals(""))
+            {
+                return "";
+            }
+            else
+            {
+                return psFecha_mdy.Substring(3, 2) + "/" + psFecha_mdy.Substring(0, 2) + "/" + psFecha_mdy.Substring(6, 4);
+            }
+        }
+
+        public static string FormatoFecha2(string psFecha_mdy)
+        {   // OBJETIVO : Modificar el formato de una fecha de MM/DD/YYYY a DD/MM/YYYY o viceversa
+            //SOLICITA  : psFecha_mdy = Fecha a formatear
+            //RETORNA   : Fecha formateada
+
+            if (psFecha_mdy.Equals(""))
+            {
+                return "";
+            }
+            else
+            {
+                return psFecha_mdy.Substring(3, 2) + "-" + psFecha_mdy.Substring(0, 2) + "-" + psFecha_mdy.Substring(6, 4);
+            }
+        }
+
+
+        public static string convertFormatDateToStringTiempo(string fechaCadena)
+        {
+            try
+            {
+                DateTime fechaDate = DateTime.Parse(fechaCadena);
+                return fechaDate.ToString("dd/MM/yyyy HH:mm");
+            }
+            catch (Exception)
+            {
+                return "Error al convertir fecha";
+            }
+        }
+
+        public static string convertFormatDateToString(string fechaCadena)
+        {
+            try
+            {
+                DateTime fechaDate = DateTime.Parse(fechaCadena);
+                return fechaDate.ToString("dd/MM/yyyy");
+            }
+            catch (Exception)
+            {
+                return "Error al convertir fecha";
+            }
+        }
+
+        public static string ObtenerDireccionIP()
+        {
+            string sHost = Dns.GetHostName();
+
+            IPHostEntry IPs = Dns.GetHostEntry(sHost);
+            IPAddress[] Direcciones = IPs.AddressList;
+
+            return Direcciones[0].ToString();
+        }
+
+        public static void PosicionarCombo(ComboBox pcCombo, string psValor)
+        {
+            bool bExisteItem = false;
+            if (psValor.Trim().Equals(""))
+            {
+                pcCombo.SelectedValue = "";
+                return;
+            }
+            for (int iIndex = 0; iIndex < pcCombo.Items.Count; iIndex++)
+            {
+                pcCombo.SelectedIndex = iIndex;
+                if (pcCombo.SelectedValue.ToString().Trim() == psValor.Trim())
+                {
+                    bExisteItem = true;
+                    break;
+                }
+            }
+            if (!bExisteItem)
+            {
+                pcCombo.SelectedValue = "";
+            }
+        }
+
+        public static void ReleaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        public static int GetIDProcces(string nameProcces)
+        {
+            try
+            {
+                Process[] asProccess = Process.GetProcessesByName(nameProcces);
+                foreach (Process pProccess in asProccess)
+                {
+                    if (pProccess.MainWindowTitle == "") { return pProccess.Id; }
+                }
+                return -1;
+            }
+            catch
+            {
+                return -1;
+            }
         }
 
         public static void PrintToExcel(DataGridView dgv )
